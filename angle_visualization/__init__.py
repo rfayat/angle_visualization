e1a45af28d3e337af6d9202f75923b97f03a231b
rfayat <r.fayat@gmail.com>
2021-04-07 12:18:44 +0200

    Extend doc and add projected plot from euclidean coordinates

--- angle_visualization/__init__.py
+++ angle_visualization/__init__.py
@@ -2,9 +2,11 @@
 import numpy as np
 import mpl_toolkits.mplot3d.axes3d as ax3d
 from .utils import grab_current_axis
+from .angle_utils import cartesian_to_latitude_longitude
 import cartopy.crs as ccrs
 from matplotlib.collections import PatchCollection
 import matplotlib.pyplot as plt
+from collections.abc import Iterable
 LAEA = ccrs.LambertAzimuthalEqualArea(central_latitude=90)
 
 
@@ -51,7 +53,44 @@ def plot_faces(faces, face_colors=None, edge_colors=None, ax=None, **kwargs):
 @grab_current_axis
 def fill_projected_faces(lat_face_all, lon_face_all, face_colors=None,
                          ax=None, **kwargs):
-    """Plot a list of faces using a Lambert azimuthal equal area projection
+    """Plot a list of faces from geographic coordinates using a LAEA projection
+
+    The faces are plotted as a matplotlib PatchCollection, each one of the
+    patches being genereted using matplotlib's `fill` function.
+
+    Parameters
+    ----------
+    lat_face_all: array, shape = (n_faces, n_vertices)
+        The latitudes of the `n_faces` vertices for the `n_faces` faces in deg
+
+    lon_face_all: array, shape = (n_faces, n_vertices)
+        The longitudes of the `n_faces` vertices for the `n_faces` faces in deg
+
+    face_colors: string, iterable of len n_faces or None (default: None)
+        The color for the faces, if face_colors is an iterable of length
+        `n_faces`, each element of face_colors is passed to `plt.fill` color.
+        Else, face_colors is used for generating each patch of the
+        PatchCollection.
+
+    ax: matplotlib axis (default: None)
+        Matplotlib axis with a cartopy LambertAzimuthalEqualArea projection. If
+        None is provided (default), the current axis is grabed.
+
+    **kwargs
+        Additional key-word arguments passed to matplotlib's fill function for
+        each facet.
+
+    Returns
+    -------
+    patch_collection: matplotlib PatchCollection
+        The resulting PatchCollection
+
+    Notes
+    -----
+    The patches overlapping the south pole and the prime meridian are ignored
+    due to rendering issue. The criterion for ignoring such facets is having
+    one vertex with latitude inferior to -85 deg and one vertex whose longitude
+    has an absolute value inferior to 5 deg.
 
     Example:
     -------
@@ -85,8 +124,9 @@ def fill_projected_faces(lat_face_all, lon_face_all, face_colors=None,
 
     """
     # Replicate the input face_color if needed
-    if face_colors is not None and len(face_colors) != len(lat_face_all):
-        face_colors = [face_colors for _ in lat_face_all]
+    n_faces = len(lat_face_all)
+    if isinstance(face_colors, Iterable) and len(face_colors) != n_faces:
+        face_colors = [face_colors] * n_faces
 
     # Loop over the coordinates of the faces
     patches = []  # list of patches that will be converted to a PatchCollection
@@ -99,4 +139,60 @@ def fill_projected_faces(lat_face_all, lon_face_all, face_colors=None,
             # Add the resulting patch to the list of patches
             patches += plt.fill(lon, lat, transform=filling_transform,
                                 color=face_colors[face_idx], **kwargs)
-    ax.add_collection(PatchCollection(patches))
+    patch_collection = PatchCollection(patches)
+    ax.add_collection(patch_collection)
+    return patch_collection
+
+
+def fill_projected_faces_euclidean(x, y, z, **kwargs):
+    """Plot a list of faces from euclidean coordinates using a LAEA projection
+
+    The x, y, z euclidean coordinates of the faces are converted to geographic
+    coordinates before using `fill_projected_faces` to plot them. See this
+    function's documentation for more information about the function's use.
+
+    Parameters
+    ----------
+    x, y, z, arrays, shape = (n_points, n_vertices)
+        The 3-dimensional euclidean coordinates of the vertices of the faces
+        that will be plotted.
+
+    **kwargs
+        Additional key-word arguments passed to `fill_projected_faces`.
+
+    Returns
+    -------
+    patch_collection: matplotlib PatchCollection
+        The resulting PatchCollection
+
+    Example:
+    -------
+    import matplotlib.pyplot as plt
+    import numpy as np
+    import angle_visualization
+    from angle_visualization.triangulation import Delaunay_Sphere
+
+    # Generate the sphere faces
+    n_points = 300
+    d = Delaunay_Sphere(n_points)
+    random_colors = [np.random.choice(["r","g","b"]) for _ in d.faces]
+
+    # Create a figure and axis with Lambert Azimuthal Equal Area projection
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1, projection=angle_visualization.LAEA)
+    ax.gridlines()
+
+    # Fill the faces of the triangulated sphere
+    angle_visualization.fill_projected_faces_euclidean(
+        *d.faces.transpose(2, 0, 1), face_colors = random_colors, alpha=.3
+    )
+
+    """
+    # Convert the euclidean coordinates to geographic coordinates
+    latlon_face_all = np.array(
+        [cartesian_to_latitude_longitude(xf, yf, zf, deg=True) for (xf, yf, zf) in zip(x, y, z)]  # noqa E501
+    )
+    # Plot the result using fill_projected_faces
+    return fill_projected_faces(latlon_face_all[:, 0, :],
+                                latlon_face_all[:, 1, :],
+                                **kwargs)
